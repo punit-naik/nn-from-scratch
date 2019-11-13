27bf6a1ec42eb2512afc3bb88556ca4ddb993bd0
Punit Naik <naik.punit44@gmail.com>
2019-11-13 20:58:28 +0530

    added recursive logic

--- src/nn_from_scratch/core.clj
+++ src/nn_from_scratch/core.clj
@@ -10,57 +10,56 @@
         Random 2-D Matrix Generator"
   [steps error-logging & rest-args]
   (let [input [[0 0 1] [0 1 1] [1 0 1] [1 1 1]]
-        expected-output [[0] [1] [1] [0]]
-        predicted-output (atom [])
-        synapse-0 (atom
-                    (-> (if (zero? (count rest-args))
-                          (create-matrix 3 4)
-                          (create-matrix 3 4 (first rest-args)))
-                        (perform-arithmetic-op 2 *)
-                        (perform-arithmetic-op 1 -)))
-        synapse-1 (atom
-                    (-> (if (zero? (count rest-args))
-                          (create-matrix 4 1)
-                          (create-matrix 4 1 (first rest-args)))
-                        (perform-arithmetic-op 2 *)
-                        (perform-arithmetic-op 1 -)))]
-    (doseq [i (range 1 (inc steps))]
-      (let [current-synapse-0 @synapse-0
-            current-synapse-1 @synapse-1
-            ; Layers
-            layer-0 input
-            layer-1 (->> (matrix-multiply layer-0 current-synapse-0)
-                         sigmoid)
-            layer-2 (->> (matrix-multiply layer-1 current-synapse-1)
-                         sigmoid)
-            ; Back Propagation
-            layer-2-error (perform-arithmetic-op expected-output layer-2 -)
-            _ (if (and error-logging
-                       (zero? (mod i (/ steps (Integer/parseInt (replace (str steps) #"0" ""))))))
-                (println "Error :" (->> (absolute layer-2-error) mean)))
-            ; Deltas
-            layer-2-delta (-> layer-2-error
-                              (perform-arithmetic-op
-                                (sigmoid layer-2 true) *))
-            layer-1-error (->> (transpose current-synapse-1)
-                               (matrix-multiply layer-2-delta))
-            layer-1-delta (-> layer-1-error
-                              (perform-arithmetic-op
-                                (sigmoid layer-1 true) *))]
-        ; Update Synapses (Weights)
-        (reset! synapse-1 (perform-arithmetic-op
-                            current-synapse-1
-                            (matrix-multiply
-                              (transpose layer-1)
-                              layer-2-delta) +))
-        (reset! synapse-0 (perform-arithmetic-op
-                            current-synapse-0
-                            (matrix-multiply
-                              (transpose layer-0)
-                              layer-1-delta) +))
-        ; Store the result in a global variable
-        (reset! predicted-output layer-2)))
-    @predicted-output))
+        expected-output [[0] [1] [1] [0]]]
+    (loop [stepz (range 1 (inc steps))
+           ;; Initial Synapses/Weights
+           synapse-0 (-> (if (zero? (count rest-args))
+                           (create-matrix 3 4)
+                           (create-matrix 3 4 (first rest-args)))
+                         (perform-arithmetic-op 2 *)
+                         (perform-arithmetic-op 1 -))
+           synapse-1 (-> (if (zero? (count rest-args))
+                           (create-matrix 4 1)
+                           (create-matrix 4 1 (first rest-args)))
+                         (perform-arithmetic-op 2 *)
+                         (perform-arithmetic-op 1 -))
+           predicted-output nil]
+      
+        (if (empty? stepz)
+          predicted-output
+          (let [;; Layers
+                layer-0 input
+                layer-1 (->> (matrix-multiply layer-0 synapse-0)
+                            sigmoid)
+                layer-2 (->> (matrix-multiply layer-1 synapse-1)
+                            sigmoid)
+                ;; Back Propagation
+                layer-2-error (perform-arithmetic-op expected-output layer-2 -)
+                _ (when (and error-logging
+                            (zero? (mod (first stepz) (/ steps (Integer/parseInt (replace (str steps) #"0" ""))))))
+                    (println "Error :" (->> (absolute layer-2-error) mean)))
+                ;; Deltas
+                layer-2-delta (-> layer-2-error
+                                  (perform-arithmetic-op
+                                  (sigmoid layer-2 true) *))
+                layer-1-error (->> (transpose synapse-1)
+                                  (matrix-multiply layer-2-delta))
+                layer-1-delta (-> layer-1-error
+                                  (perform-arithmetic-op
+                                  (sigmoid layer-1 true) *))]
+            (recur
+              (rest stepz)
+              ;; Update Synapses (Weights)
+              (perform-arithmetic-op synapse-0
+                                     (matrix-multiply
+                                      (transpose layer-0)
+                                      layer-1-delta) +)
+              (perform-arithmetic-op synapse-1
+                                     (matrix-multiply
+                                      (transpose layer-1)
+                                      layer-2-delta) +)
+              ;; Update result
+              layer-2))))))
 
 (defn train-and-print
   "Trains the Neural Network and Print the result on the console"
