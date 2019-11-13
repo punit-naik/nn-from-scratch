847e7f5f62f64917601b81dfbddf86d439ae9e48
Punit Naik <naik.punit44@gmail.com>
2019-11-13 21:20:09 +0530

    fixed tests

--- test/nn_from_scratch/core_test.clj
+++ test/nn_from_scratch/core_test.clj
@@ -5,8 +5,8 @@
 (deftest nn-purity
   (testing
    "Checking the purity of the NN functions"
-   (let [[[e-1] [e-2] [e-3] [e-4]] (train 60000 false 1000)
-         [[p-1] [p-2] [p-3] [p-4]] (train 60000 false 1000)]
+   (let [[[e-1] [e-2] [e-3] [e-4]] (:predicted (train 60000 false 1000))
+         [[p-1] [p-2] [p-3] [p-4]] (:predicted (train 60000 false 1000))]
      (are [x-1 x-2] (= x-1 x-2)
           e-1 p-1
           e-2 p-2
@@ -17,7 +17,7 @@
   (testing
    "Checking error between actual and predicted output values"
    (let [[[e-1] [e-2] [e-3] [e-4]] [[0] [1] [1] [0]]
-         [[p-1] [p-2] [p-3] [p-4]] (train 60000 false)]
+         [[p-1] [p-2] [p-3] [p-4]] (:predicted (train 60000 false))]
      (are [x-1 x-2 y] (< (Math/abs (- x-1 x-2)) y)
        e-1 p-1 0.006
        e-2 p-2 0.006
